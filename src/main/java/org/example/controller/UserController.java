f75bf6f1b59260148950f07cf8fcba2719a60a7b
Albertzry <albertzry@qq.com>
2025-03-01 14:44:03 +0800

    Spring Validation进行参数校验

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,36 +1,34 @@
 package org.example.controller;
 
+import jakarta.validation.constraints.Pattern;
 import org.example.pojo.User;
 import org.example.service.UserService;
 import org.example.pojo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
-        if (username !=null && username.length() >=5 && username.length() <= 16 &&
-                password != null && password.length() >= 5 && password.length() <= 16) {
-            //查询用户
-            User u = userService.findByUsername(username);
-            if (u == null) {
-                //用户不存在，可以注册
-                userService.register(username, password);
-                return Result.success();
-                } else {
-                //用户存在，不可以注册
-                return Result.error("用户名已存在");
-                }
-                }else{
-                    return Result.error("用户名或密码格式不正确");
-                    }
-            }
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$")String password) {
+        //查询用户
+        User u = userService.findByUsername(username);
+        if (u == null) {
+            //用户不存在，可以注册
+            userService.register(username, password);
+            return Result.success();
+        } else {
+            //用户存在，不可以注册
+            return Result.error("用户名已存在");
+        }
+    }
 }
