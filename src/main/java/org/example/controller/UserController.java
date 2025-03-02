baed7aa66cf68ae0ab32c7fbbf9a4bd7a59fc812
Albertzry <albertzry@qq.com>
2025-03-02 14:10:07 +0800

    使用拦截器进行登录验证

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -4,12 +4,16 @@ import jakarta.validation.constraints.Pattern;
 import org.example.pojo.User;
 import org.example.service.UserService;
 import org.example.pojo.Result;
+import org.example.utils.JwtUtil;
 import org.example.utils.Md5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -41,7 +45,11 @@ public class UserController {
             return Result.error("用户名不存在");
         }
         if (Md5Util.getMD5String(password).equals(loginUser.getPassword())) {
-            return Result.success("jwt token");
+            Map<String,Object> claims = new HashMap<>();
+            claims.put("id",loginUser.getId());
+            claims.put("username",loginUser.getUsername());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
         } else {
             return Result.error("密码错误");
         }
