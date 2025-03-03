764faac0480204afbaa78bc54e4b6fe533982a68
Albertzry <albertzry@qq.com>
2025-03-03 17:29:12 +0800

    文章分类以及查询

---
+++ src/main/java/org/example/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.example.pojo.Category;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    @Insert("insert into category (category_name, category_alias, create_user, create_time, update_time)" +
+            " values (#{categoryName}, #{categoryAlias}, #{createUser}, #{createTime}, #{updateTime})")
+    void add(Category category);
+
+    @Select("select * from category where create_user = #{userid}")
+    List<Category> list(Integer userid);
+}
---
+++ src/main/java/org/example/service/CategoryService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.example.pojo.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    //新增分类
+    void add(Category category);
+    //列表查询
+    List<Category> list();
+}
