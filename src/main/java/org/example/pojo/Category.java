2d8993579d45bc80b2fb79a47def4e6fcc44384e
Albertzry <albertzry@qq.com>
2025-03-04 16:59:07 +0800

    默认分组优化

--- src/main/java/org/example/pojo/Category.java
+++ src/main/java/org/example/pojo/Category.java
@@ -3,6 +3,8 @@ package org.example.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,9 +12,9 @@ import java.time.LocalDateTime;
 public class Category {
     @NotNull(groups = {Update.class})
     private Integer id;//主键ID
-    @NotEmpty(groups = {Add.class, Update.class})
+    @NotEmpty
     private String categoryName;//分类名称
-    @NotEmpty(groups = {Add.class, Update.class})
+    @NotEmpty
     private String categoryAlias;//分类别名
     private Integer createUser;//创建人ID
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -20,9 +22,9 @@ public class Category {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
 
-    public interface Add {
+    public interface Add extends Default {
     }
 
-    public interface Update {
+    public interface Update extends Default {
     }
 }
